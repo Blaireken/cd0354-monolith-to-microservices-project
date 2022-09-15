36d0f08d6b6ad356f43f0228f39c2afbd929163a
Ken Muriithi <muriithiken66@gmail.com>
2022-09-16 02:14:09 +0300

    final project

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a2ae790cf98f0441aabea569da5c62df-1433964315.us-east-2.elb.amazonaws.com/api/v0'
 };
 
 /*
